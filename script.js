ef4f0d3ab9e980e34e779a7fc4da9edf2dc3254f
EXSOSESTACIONAMIENTOUANL <2065133brian@gmail.com>
2026-03-25 08:16:00 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-database.js"></script>
 
-<script>
 //  CONFIGURACIÓN CON TUS DATOS
 const firebaseConfig = {
     apiKey: "AIzaSyBTnfeDaDYQlk3ugUHzc3SXB_b7dMrv3Qg",
